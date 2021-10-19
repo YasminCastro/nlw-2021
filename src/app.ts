cb62d973f6874e8cbbf3fd5ce33b6c0b58203c58
Yasmin <yasminsdcastro@gmail.com>
2021-10-19 15:21:56 -0300

    refactor: web-socket

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,28 @@
 import "dotenv/config";
-import express, { response } from "express";
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+import { Server } from "socket.io";
+
 import { router } from "./routes";
 
 const app = express();
+
+app.use(cors());
+//Em vez do app subir o servidor o http que irá subir. E quando subir o server http, o app tbm vai subir
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuario conectado no socket ${socket.id}`);
+});
+
 app.use(express.json());
 
 app.use(router);
@@ -21,4 +41,4 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code);
 });
 
-app.listen(4000, () => console.log("Server on Port 4000"));
+export { serverHttp, io };
