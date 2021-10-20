b17969f3dbe4eec6a342a05eb3fda6649411dd21
Yasmin <yasminsdcastro@gmail.com>
2021-10-20 11:12:31 -0300

    feat: messages from backend implemented

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,66 +1,47 @@
+import { api } from "../../services/api";
+
 import styles from "./styles.module.scss";
 
 import LogoImg from "../../assets/logo.svg";
+import { useEffect, useState } from "react";
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
 
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    //chamada para api
+    api.get<Message[]>("messages/last3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={LogoImg} alt="Logo DoWhile 2021" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Consectetur reprehenderit autem quo? Molestias pariatur eligendi,
-            dolorem nulla aliquam sequi, aperiam modi voluptatem, at ipsum
-            facilis. Aliquid, commodi numquam! Quae, accusamus.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/yasmincastro.png"
-                alt="Yasmin Castro"
-              />
-            </div>
-            <span>Yas Castro</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Consectetur reprehenderit autem quo? Molestias pariatur eligendi,
-            dolorem nulla aliquam sequi, aperiam modi voluptatem, at ipsum
-            facilis. Aliquid, commodi numquam! Quae, accusamus.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/yasmincastro.png"
-                alt="Yasmin Castro"
-              />
-            </div>
-            <span>Yas Castro</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Consectetur reprehenderit autem quo? Molestias pariatur eligendi,
-            dolorem nulla aliquam sequi, aperiam modi voluptatem, at ipsum
-            facilis. Aliquid, commodi numquam! Quae, accusamus.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/yasmincastro.png"
-                alt="Yasmin Castro"
-              />
-            </div>
-            <span>Yas Castro</span>
-          </div>
-        </li>
+        {messages.map((message) => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
