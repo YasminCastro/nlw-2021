1197173d1caa62c6f704dcfe28d8788db9ce4347
Yasmin <yasminsdcastro@gmail.com>
2021-10-20 13:50:42 -0300

    feat: butoton send message and background

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,9 +1,10 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 import { api } from "../../services/api";
 
 import styles from "./styles.module.scss";
 
 import LogoImg from "../../assets/logo.svg";
-import { useEffect, useState } from "react";
 
 type Message = {
   id: string;
@@ -14,8 +15,28 @@ type Message = {
   };
 };
 
+const messageQueue: Message[] = [];
+
+const socket = io("http://localhost:4000");
+
+socket.on("new_message", (newMessage: Message) => {
+  messageQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messageQueue.length > 0) {
+        setMessages((prevState) =>
+          [messageQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        );
+
+        messageQueue.shift;
+      }
+    }, 3000);
+  }, []);
 
   useEffect(() => {
     //chamada para api
