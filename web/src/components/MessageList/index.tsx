ce04d030ec59187dbbc79300e13a0cff5fc298f8
Yasmin <yasminsdcastro@gmail.com>
2021-10-20 10:42:28 -0300

    feat: user messges

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,3 +1,67 @@
+import styles from "./styles.module.scss";
+
+import LogoImg from "../../assets/logo.svg";
+
 export function MessageList() {
-  return <h1>Message List</h1>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={LogoImg} alt="Logo DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consectetur reprehenderit autem quo? Molestias pariatur eligendi,
+            dolorem nulla aliquam sequi, aperiam modi voluptatem, at ipsum
+            facilis. Aliquid, commodi numquam! Quae, accusamus.
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/yasmincastro.png"
+                alt="Yasmin Castro"
+              />
+            </div>
+            <span>Yas Castro</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consectetur reprehenderit autem quo? Molestias pariatur eligendi,
+            dolorem nulla aliquam sequi, aperiam modi voluptatem, at ipsum
+            facilis. Aliquid, commodi numquam! Quae, accusamus.
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/yasmincastro.png"
+                alt="Yasmin Castro"
+              />
+            </div>
+            <span>Yas Castro</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consectetur reprehenderit autem quo? Molestias pariatur eligendi,
+            dolorem nulla aliquam sequi, aperiam modi voluptatem, at ipsum
+            facilis. Aliquid, commodi numquam! Quae, accusamus.
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/yasmincastro.png"
+                alt="Yasmin Castro"
+              />
+            </div>
+            <span>Yas Castro</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
